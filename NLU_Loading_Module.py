3d6ddbfa7f9af14e2d7ffa6642ea01d4199c13d6
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-03-22 22:28:44 -0700

    Delete NLU_Loading_Module.py

--- NLU_Loading_Module.py
+++
@@ -1,8 +0,0 @@
-import time
-import sys
-
-# loading animation added for visual effect - not relevant to actual runtime of program
-def loadingAnimation():
-    for seconds in range(0, 3):
-        print(f"Processing Data{'.' * (seconds + 1)}   ", end = " ")
-        time.sleep(0.6)
